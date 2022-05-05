e75d00c63468676e21f1fa46cd2bc4045c1f2af8
Adv <tiffanydpro923@gmail.com>
2022-05-05 14:45:15 +0200

    fix bug logo shown

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";//librairie react
+import ReactDOM from "react-dom/client";//gérer la structure du site
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";//gérer la navigation du site via url et paths
 import Home from "./pages/Home/index";
 import Error from "./components/Error/index";
 import About from "./pages/About/index";
@@ -8,6 +8,8 @@ import Housing from "./pages/Housing/index";
 import "./styles/global.scss";
 import Footer from "./components/Footer/index";
 import Header from "./components/Header/index";
+
+//React.strictMode permet de mieux gérer les composants react
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
