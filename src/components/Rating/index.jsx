268388b50e1653cbac05b01b74ecde4bdba44f84
Adv <tiffanydpro923@gmail.com>
2022-05-06 13:36:43 +0200

    maj comments for tags

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -5,14 +5,14 @@ function Rating({ rating }) {
   let content = [];
   if (rating < 5) {
     for (let i = 0; i < rating; i++) {
-      content.push(<img key={`key${i}`} src={fullStar} alt="star" />);
+      content.push(<img key={`key${i}`} src={fullStar} alt="star" />);//remplir en orange les étoiles de 1 à rating (= note attribuée de 1 à 5 dans le data.json)
     }
     for (let i = rating; i < 5; i++) {
-      content.push(<img key={`key${i}`} src={emptyStar} alt="star" />);
+      content.push(<img key={`key${i}`} src={emptyStar} alt="star" />);//étoile vide pour les étoiles restantes au dessus de rating jusqu'à 5 inclus
     }
   } else {
     for (let i = 0; i < rating; i++) {
-      content.push(<img key={`key${i}`} src={fullStar} alt="star" />);
+      content.push(<img key={`key${i}`} src={fullStar} alt="star" />);//remplir en plein supplémentaire par mesure de sécurité
     }
   }
 
--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,12 @@ import "./styles/global.scss";
 import Footer from "./components/Footer/index";
 import Header from "./components/Header/index";
 
-//React.strictMode permet de mieux gérer les composants react
+//React.strictMode permet de mieux gérer les composants react et d'avoir des fonctions pures qui retournent toujours la meme chose à chaque appel (toujours le même state, exemple : cliquer sur un bouton pour ajouter 100 retourne toujours 100)
+
+//props = propriété ou objet unique qui sert à stocker les valeurs des attributs d'une étiquette (tag)
+
+//props VS state
+//props est passé au composant (à la manière des arguments d'une fonction) tandis que state est géré dans le composant (comme le sont les variables déclarées à l'intérieur d'une fonction).
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
